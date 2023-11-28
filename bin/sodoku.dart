54b6589f8e67063c830d7be23c87061945de0f24
bukh-sal <ssyb2014@gmail.com>
2023-11-28 20:11:10 +0300

    Update sodoku.dart

--- bin/sodoku.dart
+++ bin/sodoku.dart
@@ -1,7 +1,8 @@
-import 'dart:math';
+import "dart:io";
 
 class Grid {
   late List<List<int>> grid;
+  late List<List<Map<int, int>>> locationsMap;
 
   Grid() {
     grid = [
@@ -15,6 +16,8 @@ class Grid {
       [1, 2, 3, 5, 0, 8, 0, 6, 9],
       [0, 4, 0, 2, 0, 7, 0, 0, 0],
     ];
+
+    locationsMap = generateLocationsMap();
   }
 
   @override
@@ -22,11 +25,51 @@ class Grid {
     return "Grid";
   }
 
+  List<List<Map<int, int>>> generateLocationsMap() {
+    // generate the locations for the entire grid
+    List<List<Map<int, int>>> segmentLocationsMap = [];
+    List<Map<int, int>> rowLocations;
+
+    for (int rowIdx = 0; rowIdx < grid.length; rowIdx++) {
+      rowLocations = [];
+      for (int colIdx = 0; colIdx < grid[rowIdx].length; colIdx++) {
+        rowLocations.add({rowIdx: colIdx});
+      }
+      segmentLocationsMap.add(rowLocations);
+    }
+
+    return segmentLocationsMap;
+  }
+
+
   void printGrid() {
     for (int i = 0; i < grid.length; i++) {
       print(grid[i]);
     }
   }
+
+
+  void displayGrid() {
+    for (int rowIdx = 0; rowIdx < grid.length; rowIdx++) {
+      List<int> row = grid[rowIdx];
+      for (int colIdx = 0; colIdx < row.length; colIdx++) {
+        int value = row[colIdx];
+        stdout.write('$value ');
+
+        if (colIdx == 2 || colIdx == 5) {
+          stdout.write('  ');
+        }
+      }
+
+      if (rowIdx == 2 || rowIdx == 5) {
+        stdout.write('\n\n');
+      } else {
+        stdout.write('\n');
+      }
+
+    }
+  }
+
 
   List<int> getValuesInRow(int rowNumber) {
     List<int> uVals = [];
@@ -40,6 +83,7 @@ class Grid {
     return uVals;
   }
 
+
   List<int> getValuesInCol(int colNumber) {
     List<int> uVals = [];
     for (int i = 0; i < grid.length; i++) {
@@ -52,6 +96,7 @@ class Grid {
     return uVals;
   }
 
+
   List<int> getValuesInSegment(int locX, int locY) {
     Map<int, int> startFromMap = {0: 0, 1: 3, 2: 6};
     int rowsStartFrom = startFromMap[locY]!;
@@ -62,7 +107,6 @@ class Grid {
       List<int> rowValues = rows[i].sublist(colsStartFrom, colsStartFrom + 3);
       rows[i] = rowValues;
     }
-    print(rows);
 
     List<int> uVals = [];
     for (int a = 0; a < rows.length; a++) {
@@ -79,38 +123,54 @@ class Grid {
     return uVals;
   }
 
+
+  List<Map<int, int>> getSegmentLocations(int locX, int locY) {
+    // reduce the locations to the desired segments
+    Map<int, int> startFromMap = {0: 0, 1: 3, 2: 6};
+    List<Map<int, int>> combined = [];
+
+    int rowsStartFrom = startFromMap[locY]!;
+    int colsStartFrom = startFromMap[locX]!;
+    List<List<Map<int, int>>> rows = locationsMap.sublist(rowsStartFrom, rowsStartFrom + 3);
+
+    for (int i = 0; i < rows.length; i++) {
+      List<Map<int, int>> rowValues =
+          rows[i].sublist(colsStartFrom, colsStartFrom + 3);
+      combined.addAll(rowValues);
+    }
+
+    return combined;
+  }
+
+
   List<int> getBlockSoloutionSpace(int locX, int locY) {
-    List<int> soloutionSpace = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    Map<int, int> blockSegmentMap = {
-      0: 0,
-      1: 0,
-      2: 0,
-      3: 1,
-      4: 1,
-      5: 1,
-      6: 2,
-      7: 2,
-      8: 2,
-    };
-
-    int segX = ((locX) / 3).floor();
-    int segY = ((locY) / 3).floor();
-    List<int> segmentValues = getValuesInSegment(segX, segY);
-    List<int> rowValues = getValuesInRow(locX);
-    List<int> colValues = getValuesInCol(locY);
-
     int currentValue = grid[locY][locX];
     if (currentValue != 0) {
       List<int> cv = [currentValue];
       return cv;
     }
 
+    List<int> soloutionSpace = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    Map<int, int> blockSegmentMap = {
+      0: 0, 1: 0, 2: 0,
+      3: 1, 4: 1, 5: 1,
+      6: 2, 7: 2, 8: 2
+    };
+
+    // Getting values in segment, row, and column
+    int segX = blockSegmentMap[locX]!;
+    int segY = blockSegmentMap[locY]!;
+    List<int> segmentValues = getValuesInSegment(segX, segY);
+    List<int> rowValues = getValuesInRow(locY);
+    List<int> colValues = getValuesInCol(locX);
+
+    // combine segment, row, and column into one list
     List<int> conflictingValues = [];
     conflictingValues.addAll(segmentValues);
     conflictingValues.addAll(rowValues);
     conflictingValues.addAll(colValues);
 
+    // remove conflicting values from soloution space
     for (int i = 0; i < conflictingValues.length; i++) {
       int value = conflictingValues[i];
       if (soloutionSpace.contains(value)) {
@@ -121,17 +181,49 @@ class Grid {
     return soloutionSpace;
   }
 
+  void singleOptionPass() {
+    for (int y = 0; y < 9; y++) {
+      for (int x = 0; x < 9; x++) {
+        List<int> solSpace = getBlockSoloutionSpace(x, y);
+        if (solSpace.length == 1) {
+          grid[y][x] = solSpace[0];
+        }
+      }
+    }
+  }
+
+  void runPass() {
+    singleOptionPass();
+  }
+
+  int countEmpty() {
+    int zeroCount = 0;
+
+    for (int y = 0; y < 9; y++) {
+      for (int x = 0; x < 9; x++) {
+        if (grid[y][x] == 0) {
+          zeroCount++;
+        }
+      }
+    }
+
+    return zeroCount;
+  }
+
   // END OF CLASS
 }
 
 void main(List<String> arguments) {
   Grid grid = Grid();
-  grid.printGrid();
-  print("Values in row 0 ${grid.getValuesInRow(0)}");
-  print("Values in col 0 ${grid.getValuesInCol(0)}");
-  print("Values in segment x=0, y=0 ${grid.getValuesInSegment(0, 0)}");
+  print('space for row 4 col 1 ${grid.getBlockSoloutionSpace(0, 3)}');
 
-  print('Block Soloution Space ${grid.getBlockSoloutionSpace(2, 8)}');
+  grid.displayGrid();
+  print("Zero count = ${grid.countEmpty()}");
+  grid.runPass();
+  print('\n\n');
+  grid.displayGrid();
+  print("Zero count = ${grid.countEmpty()}");
 
-  print('soloution space ${grid.getBlockSoloutionSpace(1, 0)}');
+  var v = grid.getSegmentLocations(0, 0);
+  print(v);
 }
